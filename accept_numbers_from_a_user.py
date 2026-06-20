a0d717a3dff0e630be149cb2a70339a99dc67df8
Shayan <shayan6166213@gmail.com>
2026-06-19 20:04:05 -1200

    Delete accept_numbers_from_a_user.py

--- accept_numbers_from_a_user.py
+++
@@ -1,6 +0,0 @@
-first_number = input("Enter First number:")
-second_number = input("Enter Second number:")
-
-multiplication = int(first_number) * int(second_number)
-print("Multiplication of numbers is: ", multiplication)
-print('Name', 'is', 'James', sep='***')
