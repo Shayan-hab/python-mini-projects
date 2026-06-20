8799a5b32555b1705383c64611fbbcc8a86c44fc
Shayan-hab <shayan6166213@gmail.com>
2026-06-19 19:24:16 -1200

    first_practice

---
+++ accept_numbers_from_a_user.py
@@ -0,0 +1,6 @@
+first_number = input("Enter First number:")
+second_number = input("Enter Second number:")
+
+multiplication = int(first_number) * int(second_number)
+print("Multiplication of numbers is: ", multiplication)
+print('Name', 'is', 'James', sep='***')
